a2d1e3a6d1de78ed512d68969670217008508836
Jhonathan Geovanny Llanos Gonzalez <jhonathan.llanos@udla.edu.ec>
2020-12-12 01:08:05 -0500

    Primer Commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGeolocation from 'vue-browser-geolocation'
 
 Vue.config.productionTip = false
+Vue.use(VueGeolocation)
+
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCns9qTHyD7PLX0FrIJOWXWKKaBLgHU-ec'
+  }
+})
 
 new Vue({
   render: function (h) { return h(App) },
